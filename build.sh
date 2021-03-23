5324acd5e66341b58ede871f710ae5116c7882f9
jameswilddev <jameswilddev@outlook.com>
2021-03-23 18:51:38 +0000

    Add build scripts.

---
+++ build.sh
@@ -0,0 +1,38 @@
+set -e
+
+sudo apt install openjdk-8-jre -y
+
+rm -rf ./build
+
+mkdir -p ./build/unpacked-dependencies/avr
+cp ./submodules/arduino/Arduino/build/linux/avr_tools_linux64.tar.bz2 ./build/unpacked-dependencies/avr
+bunzip2 -dk ./build/unpacked-dependencies/avr/avr_tools_linux64.tar.bz2
+tar -xvf ./build/unpacked-dependencies/avr/avr_tools_linux64.tar -C ./build/unpacked-dependencies/avr
+PATH=$PATH:${PWD}/build/unpacked-dependencies/avr/tools/avr/bin
+
+mkdir -p ./build/games/ballblaster
+cp -r ./submodules/rossumur/Arduinocade/examples/ballblaster ./build/games
+cp ./submodules/rossumur/Arduinocade/*.cpp ./build/games/ballblaster
+cp ./submodules/rossumur/Arduinocade/*.h ./build/games/ballblaster
+xvfb-run -a ./submodules/arduino/Arduino/build/linux/work/arduino --verify --board arduino:avr:uno --pref build.path=${PWD}/build/games/ballblaster --verbose ./build/games/ballblaster/ballblaster.ino
+
+mkdir -p ./build/games/jowst
+cp -r ./submodules/rossumur/Arduinocade/examples/jowst ./build/games
+cp ./submodules/rossumur/Arduinocade/*.cpp ./build/games/jowst
+cp ./submodules/rossumur/Arduinocade/*.h ./build/games/jowst
+sed -i 's/^#define BALLBLASTER_GAME$//g' ./build/games/jowst/config.h
+xvfb-run -a ./submodules/arduino/Arduino/build/linux/work/arduino --verify --board arduino:avr:uno --pref build.path=${PWD}/build/games/jowst --verbose ./build/games/jowst/jowst.ino
+
+mkdir -p ./build/games/pacman
+cp -r ./submodules/rossumur/Arduinocade/examples/pacman ./build/games
+cp ./submodules/rossumur/Arduinocade/*.cpp ./build/games/pacman
+cp ./submodules/rossumur/Arduinocade/*.h ./build/games/pacman
+sed -i 's/^#define BALLBLASTER_GAME$//g' ./build/games/pacman/config.h
+xvfb-run -a ./submodules/arduino/Arduino/build/linux/work/arduino --verify --board arduino:avr:uno --pref build.path=${PWD}/build/games/pacman --verbose ./build/games/pacman/pacman.ino
+
+mkdir -p ./build/games/titan
+cp -r ./submodules/rossumur/Arduinocade/examples/titan ./build/games
+cp ./submodules/rossumur/Arduinocade/*.cpp ./build/games/titan
+cp ./submodules/rossumur/Arduinocade/*.h ./build/games/titan
+sed -i 's/^#define BALLBLASTER_GAME$//g' ./build/games/titan/config.h
+xvfb-run -a ./submodules/arduino/Arduino/build/linux/work/arduino --verify --board arduino:avr:uno --pref build.path=${PWD}/build/games/titan --verbose ./build/games/titan/titan.ino
---
+++ readme.md
@@ -0,0 +1,24 @@
+# neomura/arduinocade-patches
+
+Unofficial patches to [Arduinocade](https://github.com/rossumur/Arduinocade) to make its games compatible with the [Neomura ATMega328P Cartridge](https://github.com/neomura/atmega328p-cartridge).
+
+## License
+
+While this repository is [MIT licensed](./license.md), it includes Git submodules of:
+
+| Repository                                                      | License    |
+| --------------------------------------------------------------- | ---------- |
+| [rossumur/Arduinocade](https://github.com/rossumur/Arduinocade) | Unlicensed |
+| [arduino/Arduino](https://github.com/arduino/Arduino)           | GPL/LGPL   |
+
+You should make your own checks to ensure that your usage of this repository is valid within these license agreements.
+
+## Required hardware modifications
+
+X1 must be changed to a 28.31818MHz crystal for these games to work at full speed on a real display.
+
+You may find that your ATMega328Ps do not function at this clock speed; not all do.
+
+## Changes made by these patches
+
+TODO
